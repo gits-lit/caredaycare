de35b57609b981dfe125b336e5d647dab0374f0e
Clark <clarkphan@github.com>
2018-02-22 23:01:55 -0800

    add dispense pill intent

--- index.js
+++ index.js
@@ -1,61 +1,86 @@
 // configure environment variables in .env
-require('dotenv').config();
+require('dotenv').config()
 
 // import alexa software development kit
-const Alexa = require('alexa-sdk');
+const Alexa = require('alexa-sdk')
 
 // import aws software development kit for our database
-const awsSDK = require('aws-sdk');
+const awsSDK = require('aws-sdk')
 awsSDK.config.update({
   region: 'us-east-1',
   endpoint: 'https://dynamodb.us-east-1.amazonaws.com',
   accessKeyId: process.env.KEY,
   secretAccessKey: process.env.SECRET_KEY
-});
+})
 
 // start an instance of our DynamoDB
-const docClient = new awsSDK.DynamoDB.DocumentClient();
+const docClient = new awsSDK.DynamoDB.DocumentClient()
 
-const table = "PillsDB";
+const table = 'PillsDB'
 
-const count = 10;
-const pill_brand = "generic";
+const pill_brand = 'generic'
 
 const params = {
-  TableName:table,
-  Key:{
-    "pill_count": count,
-    "pill_brand": pill_brand
+  TableName: table,
+  Key: {
+    'pill_brand': pill_brand
   }
-};
+}
 
-exports.handler = function(event, context, callback) {
-    // create alexa object
-    const alexa = Alexa.handler(event, context, callback);
-    // our application id
-    alexa.appId = process.env.APP_ID;
-    // register intents for our skill
-    alexa.registerHandlers(handlers);
-    // after configuration, run alexa
-    alexa.execute();
-};
+exports.handler = function (event, context, callback) {
+  // create alexa object
+  const alexa = Alexa.handler(event, context, callback)
+  // our application id
+  alexa.appId = process.env.APP_ID
+  // register intents for our skill
+  alexa.registerHandlers(handlers)
+  // after configuration, run alexa
+  alexa.execute()
+}
 
 const handlers = {
-  'HelloWorldIntent' : function() {
-    //emit response directly
-    this.emit(':tell', 'Hello World!');
+  'HelloWorldIntent': function () {
+    // emit response directly
+    this.emit(':tell', 'Hello World!')
   },
-  'PillCountIntent' : function() {
-    const self = this;
-    docClient.get(params, function(err, data) {
+  'PillCountIntent': function () {
+    const self = this
+    docClient.get(params, function (err, data) {
       if (err) {
-          console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2))
       } else {
-          console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-          //emit response directly
-          self.emit(':tell', 'You currently have ' + data.Item.pill_count + ' pills');
+        console.log('GetItem succeeded:', JSON.stringify(data, null, 2))
+        // emit response directly
+        self.emit(':tell', 'You currently have ' + data.Item.pill_count + ' pills')
       }
-    }); 
-  }
-};
+    })
+  },
+  'PillDispenseIntent': function () {
+    const self = this
+    docClient.get(params, function (err, data) {
+      if (err) {
+        console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2))
+      } else {
+        console.log('GetItem succeeded:', JSON.stringify(data, null, 2))
+        // add update query
+        params.UpdateExpression = 'set pill_count = :new_count'
+        params.ExpressionAttributeValues = {
+          ':new_count': data.Item.pill_count-1
+        }
+        params.ReturnValues = 'UPDATED_NEW'
+        console.log('Updating the item...')
 
+        // update the database
+        docClient.update(params, function (err, data) {
+          if (err) {
+            console.error('Unable to update item. Error JSON:', JSON.stringify(err, null, 2))
+          } else {
+            console.log('UpdateItem succeeded:', JSON.stringify(data, null, 2))
+            // emit response directly
+            self.emit(':tell', 'Pill dispensed. You now have ' + data.Attributes.pill_count + ' pills')
+          }
+        })
+      }
+    })
+  }
+}
