e6e2e49db8bffb14423ac18961c6c5720efb1b82
Emily Nguyen <emilyhuongnguyen@gmail.com>
2018-02-26 16:44:10 -0800

    Add color checks and fix intensity values

--- src/PartTesters/color.py
+++ src/PartTesters/color.py
@@ -6,7 +6,6 @@ File: color.py
 Author: Antony Nguyen
 Description: A program to test the color recognition sensor
              TCS230 TCS3200 when connected to an Arduino
-             using Nanpy
 More Credit: Edgaras Art for original Arduino code which
              I translated into python and modified.
              https://www.youtube.com/watch?v=BERHEVgaX40
@@ -32,47 +31,65 @@ s3 = 11
 out = 10
 
 # Color Variables
-r = 0
-g = 0
-b = 0
-colors = [r, g, b]
+red = 0
+green = 0
+blue = 0
+colors = [red, green, blue]
 
-a = Arduino('9600')
+# Setup the Arduino
+
+Arduino = Arduino('9600')
 
 # Setup the pinModes
-a.pinMode(s0, "OUTPUT")
-a.pinMode(s1, "OUTPUT")
-a.pinMode(s2, "OUTPUT")
-a.pinMode(s3, "OUTPUT")
-a.pinMode(out, "INPUT")
-a.digitalWrite(s0, "HIGH")
-a.digitalWrite(s1, "HIGH")
+Arduino.pinMode(s0, 'OUTPUT')
+Arduino.pinMode(s1, 'OUTPUT')
+Arduino.pinMode(s2, 'OUTPUT')
+Arduino.pinMode(s3, 'OUTPUT')
+Arduino.pinMode(out, 'INPUT')
+Arduino.digitalWrite(s0, 'HIGH')
+Arduino.digitalWrite(s1, 'HIGH')
 
 # Color
 def color():
-    a.digitalWrite(s2, "LOW")
-    a.digitalWrite(s3, "LOW")
+    Arduino.digitalWrite(s2, 'LOW')
+    Arduino.digitalWrite(s3, 'LOW')
     # count OUT, pRed, RED
-    r = a.pulseIn(out, "HIGH")
-    a.digitalWrite(s3, "HIGH")
+    if(Arduino.digitalRead(out) == 1):
+      lorh = 'LOW'
+    else:
+      lorh = 'HIGH'
+    r = Arduino.pulseIn(out, lorh)
+    Arduino.digitalWrite(s3, 'HIGH')
     # count OUT, pBlue, BLUE
-    b = a.pulseIn(out, "HIGH")
-    a.digitalWrite(s2, "HIGH")
+    if(Arduino.digitalRead(out) == 1):
+      lorh = 'LOW'
+    else:
+      lorh = 'HIGH'
+    b = Arduino.pulseIn(out, lorh)
+    Arduino.digitalWrite(s2, 'HIGH')
+    if(Arduino.digitalRead(out) == 1):
+      lorh = 'LOW'
+    else:
+      lorh = 'HIGH'
     # count OUT, pGreen, GREEN
-    g = a.pulseIn(out, "HIGH")
+    g = Arduino.pulseIn(out, lorh)
     return [r, g, b] 
+
 
 # Main execution loop
 while True:
     colors = color()
-    r = colors[0]
-    g = colors[1]
-    b = colors[2]
-    print("R: ")
-    print(r)
-    print("G: ")
-    print(g)
-    print("B: ")
-    print(b)  
+    red = colors[0]
+    green = colors[1]
+    blue = colors[2]
+    print(red)
+    print(green)
+    print(blue)
+    if(red < blue and red < green and red < 20):
+        print('Red')
+    elif(blue < red and blue < green and blue < 20):
+        print('Blue')
+    elif(green < red and green < blue and green < 20):
+        print('Green')
     time.sleep(.5)
-    print("\nClosing program...")
+print("\nClosing program...")
