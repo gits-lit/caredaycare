e5987ceffe21541797b5d8bb7c274b22ddb6f118
ClarkPhan <clarkphan24@gmail.com>
2018-03-07 14:07:23 -0800

    add alexa route for server

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const express = require("express");
 // Middle-ware to test client requests
 const bodyParser = require('body-parser');
 
+// lambda function
+const index = require('./index.js');
+
+// lambda mock context
+const context = require('aws-lambda-mock-context');
+
 // Initialize Express
 const app = express();
 
@@ -23,4 +29,12 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.get('/', (req, res) => {
     res.send("test");
+});
+
+app.post('/alexa/', function (req, res) {
+    let ctx = context();
+    index.handler(req.body,ctx);
+    ctx.Promise
+        .then(resp => {  return res.status(200).json(resp); })
+        .catch(err => {  console.log(err); })
 });
