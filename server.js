e036bac9286320bd8d4c770bd6ef123077f1fb02
Clark <clarkphan@github.com>
2018-03-07 18:02:10 -0800

    import alexa commands

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 // logging client request (POST, GET, etc..)
 const logger = require("morgan");
+// Middle-ware to test client requests
+const bodyParser = require('body-parser');
 
 // express server
 const express = require("express");
-
-// Middle-ware to test client requests
-const bodyParser = require('body-parser');
 
 // path to join files
 const path = require('path');
 
 // lambda function
 const index = require('./index.js');
-
-// lambda mock context
 const context = require('aws-lambda-mock-context');
+
+// import alexa commands
+const alexaCommands = require('./commands.json');
 
 // Initialize Express
 const app = express();
 
 // parse application/json
 app.use(bodyParser.json());
-
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname, "public")));
@@ -38,9 +37,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.post('/alexa/', function (req, res) {
+app.get('/alexa/', function (req, res) {
     let ctx = context();
-    index.handler(req.body,ctx);
+    //index.handler(req.body, ctx);
+    index.handler(alexaCommands[0], ctx);
     ctx.Promise
         .then(resp => {  return res.status(200).json(resp); })
         .catch(err => {  console.log(err); })
