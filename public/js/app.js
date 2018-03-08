56288f6bb701ed7b92ca4750a4f0c50b72eb362c
ClarkPhan <clarkphan24@gmail.com>
2018-03-07 19:12:57 -0800

    add updateCount function for pills

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,17 @@
 $(document).ready(() => {
+  let updateCount = () => {
+    $.ajax({
+      method: "GET",
+      url: "/alexa/pillCount"
+    }).done(res => {
+      $('#red-count').html(res[0]);
+    });
+  }
+
   $('#dispenseBtn').on('click', () => {
     console.log('test');
   });
+
+  //update pill count on page load
+  updateCount();
 })
