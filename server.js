a0624044833904d941f78c6b07937f3ec3e72a35
Clark <clarkphan@github.com>
2018-03-11 08:16:57 -0700

    add code to dispense at certain angles

--- server.js
+++ server.js
@@ -17,21 +17,41 @@ app.use(apiRoutes);
 
 // listening for routes
 app.listen(process.env.PORT || 3000, () => {
-    console.log("App is running on port 3000!");
+  console.log("App is running on port 3000!");
 });
 
-//add python code execution
-const spawn = require('child_process').spawn;
-const ls = spawn('python', ['test.py', 'arg1', 'arg2']);
+app.get('/dispenseRedPill', (req, res) => {
+  //add python code execution for 10 degree turn
+  const spawn = require('child_process').spawn;
+  const ls = spawn('python', ['./src/servo.py', '2', '10']);
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+  ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 });
 
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
+app.get('/dispenseGreenPill', (req, res) => {
+  //add python code execution for 10 degree turn
+  const spawn = require('child_process').spawn;
+  const ls = spawn('python', ['./src/servo.py', '2', '180']);
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+  ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 });
